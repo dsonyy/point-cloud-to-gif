d94442a1c708e5f805ea1c6dcfa53905a1eb281b
dsonyy <dsonyyy@gmail.com>
2018-05-12 23:33:08 +0200

    work finished today

--- pc2p.py
+++ pc2p.py
@@ -1,3 +1,29 @@
-import pypng
+import sys
+import os
+import png
 
+def main():
+    if len(sys.argv) < 3:
+        print("Too few arguments passed. Please try this:")
+        print(" " + os.path.basename(__file__) + " <input>  <output>")
+        print(" <input>  -- point cloud filename (read only)")
+        print(" <output> -- target png filename (for write)")
+        return
+    pc_path = sys.argv[1]
+    png_path = sys.argv[2]    
+
+    pc = []
+    with open(pc_path, "r") as f:
+        for line in f:
+            point = []
+            for num in line.split():
+                point.append(num)
+            pc.append(point)                   
+
+    png_file = open(png_path, "wb")
+    print(pc)
+
+
+if __name__ == "__main__":
+	main()
 
