72bbbdec74cd0ad7d0e20e3bec547200eefcfcb9
dsonyy <dsonyyy@gmail.com>
2018-05-13 16:15:09 +0200

    pillow

--- pc2p.py
+++ pc2p.py
@@ -1,10 +1,37 @@
 import sys
 import os
-import png
+from PIL import Image
 
 w = 640
 h = 480
 offset = 40
+
+def file_to_pc(filename, echo = True):
+    pc = []
+    try:
+        with open(filename, "r") as f:
+            for line in f:
+                point = []
+                for num in line.split():
+                    point.append(int(num))
+                pc.append(point)
+    except:
+        print("Unable to load input point cloud")
+    if echo == True:
+        print(pc)
+    return pc
+	
+def pc_to_png(filename):
+    try:
+        image = Image.open(filename)
+    except:
+        print("Unable to load output image.")
+        return
+    image.load()
+
+
+
+
 
 def main():
     if len(sys.argv) < 3:
@@ -13,21 +40,8 @@ def main():
         print(" <input>  -- point cloud filename (read only)")
         print(" <output> -- target png filename (for write)")
         return
-    pc_path = sys.argv[1]
-    png_path = sys.argv[2]    
 
-    pc = []
-    with open(pc_path, "r") as f:
-        for line in f:
-            point = []
-            for num in line.split():
-                point.append(int(num))
-            pc.append(point)                   
-	
-
-    png_file = open(png_path, "wb")
-    print(pc)
-
+    file_to_pc(sys.argv[1])
 
 if __name__ == "__main__":
 	main()
