f3c2f0d8be777200d35ae25aa54ddbca12f021aa
dsonyy <dsonyyy@gmail.com>
2018-12-15 16:30:48 +0100

    sorting points // shaders

--- pc2png.py
+++ pc2png.py
@@ -3,20 +3,23 @@ import sys
 import math
 
 xyz = []
-#try:
-data = open(sys.argv[1], "r").readlines()
-for line in data:
-    line = line.replace("\n", "")
-    point = []
-    for num in line.split(";"):
-        point.append(float(num))
-    xyz.append(point)
+try:
+    data = open(sys.argv[1], "r").readlines()
+    for line in data:
+        line = line.replace("\n", "")
+        point = []
+        for num in line.split(";"):
+            try:
+                point.append(float(num))
+            except:
+                break
+        if len(point) == 3:
+            xyz.append(point)
+except:
+     print("Unable to load input point cloud")
+     sys.exit()
 
-# except:
-#     print("Unable to load input point cloud")
-#     sys.exit()
-
-xyz.sort(key = lambda x: x[2], reverse=True)
+xyz.sort(key = lambda x: [x[2], x[1], x[0]])
 
 # normalize
 min_x = min(i[0] for i in xyz)
@@ -41,20 +44,37 @@ pygame.display.flip()
 
 
 # render figure
-min_x, max_x = min(i[0] for i in xyz), max(i[0] for i in xyz)
-min_y, max_y = min(i[1] for i in xyz), max(i[1] for i in xyz)
-min_z, max_z = min(i[2] for i in xyz), max(i[2] for i in xyz)
+max_x = max(i[0] for i in xyz)
+max_y = max(i[1] for i in xyz)
+max_z = max(i[2] for i in xyz)
+center_x = max_x / 2
+center_y = max_y / 2
+center_z = max_z / 2
 
-X = math.fabs(min_x - max_x)
-Y = math.fabs(min_y - max_y)
-Z = math.fabs(min_z - max_z)
-
-P_WIDTH = 3
-P_HEIGHT = 3
+P_WIDTH = 2
+P_HEIGHT = 2
 K = 3
 
+def distance(A, B = [center_x, center_y, center_z]):
+    return math.sqrt((A[0] - B[0])**2 + (A[1] - B[1])**2 + (A[2] - B[2])**2)
+
 def get_color(point):
-    return [255/X*point[0]%256, 255/Y*point[1]%256, 255/Z*point[2]%256]
+    repeat = 1
+
+    r = math.floor(255 * 1 / (max_x) * point[0] % 510)
+    g = r #math.floor(255 * repeat / (max_y) * point[1] % 510)
+    b = math.floor(255 * repeat / (max_z) * point[2] % 510)
+
+    if r <= 255: r = 255 - r
+    elif r > 255: r = r - 255
+
+    # if g <= 255: g = 255 - g
+    # elif g > 255: g = g - 255
+
+    # if b <= 255: b = 255 - b
+    # elif b > 255: b = b - 255
+
+    return [255 / 100 *distance(point), 255 / 100 *distance(point), 255 / 100 *distance(point)]
     
 def get_rect(point):
     x = point[0] * K
